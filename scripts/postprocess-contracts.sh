1a516491401bee96b38cbd578a909dca1c520276
John Hilliard <praetoriansentry@gmail.com>
2025-02-05 14:53:51 -0500

    feat: using foundry to try to build contracts

---
+++ scripts/postprocess-contracts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ ! -d compiled-contracts ]]; then
+    echo "It looks like you are not executing this script from the e2e root directory" >&2
+    exit 1
+fi
+
+find compiled-contracts -mindepth 1 -type d -name '*.sol' | while read contract; do
+    echo $contract
+    contract_name=$(echo $contract | sed 's/compiled-contracts\/\(.*\).sol/\1/')
+    echo $contract_name
+    lower_contract_name=$(echo $contract_name | tr '[:upper:]' '[:lower:]')
+    cat compiled-contracts/$contract_name.sol/$contract_name.json | jq -r '.abi' > core/contracts/abi/$lower_contract_name.abi
+    cat compiled-contracts/$contract_name.sol/$contract_name.json | jq -r '.bytecode.object' | sed 's/^0x//'  > core/contracts/bin/$lower_contract_name.bin
+done
