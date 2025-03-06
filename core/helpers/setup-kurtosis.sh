3cdf7534d7229d3cdbd6d1d971acac16f940e35a
dan moore <dmoore@polygon.technology>
2025-03-06 12:28:11 +0100

    e2e setup kurtosis debug

--- core/helpers/setup-kurtosis.sh
+++ core/helpers/setup-kurtosis.sh
@@ -59,8 +59,8 @@ else
 
     # ✅ Download the default config
     ENCLAVE="cdk"
-    VERSION="main"
-    COMBINATIONS_FILE="https://raw.githubusercontent.com/0xPolygon/kurtosis-cdk/refs/heads/${VERSION}/.github/tests/combinations/${NETWORK}.yml"
+    VERSION="dan/agglayer_toml_fix"
+    COMBINATIONS_FILE="https://raw.githubusercontent.com/0xPolygon/kurtosis-cdk/refs/heads/main/.github/tests/combinations/${NETWORK}.yml"
     
     CONFIG_FILE=$(mktemp)
     curl -sSL "${COMBINATIONS_FILE}" -o "${CONFIG_FILE}"
