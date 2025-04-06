57e1c1b74bc070b535a05110032581e68547782d
Arpit Temani <temaniarpit27@gmail.com>
2025-04-06 16:40:53 -0700

    fix

--- core/helpers/common-setup.bash
+++ core/helpers/common-setup.bash
@@ -139,11 +139,21 @@ _common_setup() {
     local combined_json_file="/opt/zkevm/combined.json"
     combined_json_output=$($CONTRACTS_SERVICE_WRAPPER "cat $combined_json_file")
     bridge_addr=$(echo "$combined_json_output" | jq -r .polygonZkEVMBridgeAddress)
+    if echo "$combined_json_output" | jq empty > /dev/null 2>&1; then
+        bridge_addr=$(echo "$combined_json_output" | jq -r .polygonZkEVMBridgeAddress)
+    else
+        bridge_addr=$(echo "$combined_json_output" | tail -n +2 | jq -r .polygonZkEVMBridgeAddress)
+    fi
     echo "Bridge address=$bridge_addr" >&3
 
     local rollup_params_file="/opt/zkevm/create_rollup_parameters.json"
     rollup_params_output=$($CONTRACTS_SERVICE_WRAPPER "cat $rollup_params_file")
     gas_token_addr=$(echo "$rollup_params_output" | jq -r .gasTokenAddress)
+    if echo "$gas_token_addr" | jq empty > /dev/null 2>&1; then
+        gas_token_addr=$(echo "$rollup_params_output" | jq -r .gasTokenAddress)
+    else
+        gas_token_addr=$(echo "$rollup_params_output" | tail -n +2 | jq -r .gasTokenAddress)
+    fi
     echo "Gas token address=$gas_token_addr" >&3
 
     readonly sender_private_key=${SENDER_PRIVATE_KEY:-"12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625"}
