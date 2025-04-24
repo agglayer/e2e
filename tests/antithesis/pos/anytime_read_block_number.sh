2aa5ca7dd7d94796f2f417e7e9b5924e42c437e5
leovct <l009.vincent@gmail.com>
2025-04-24 14:08:38 +0200

    feat: update antithesis tests

--- tests/antithesis/pos/anytime_read_block_number.sh
+++ tests/antithesis/pos/anytime_read_block_number.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-cast block-number --rpc-url "${L2_RPC_URL}"
+# Script timeout duration, in seconds.
+timeout_seconds=30
+
+# Retry loop to avoid issues with network partitions.
+start_time=$(date +%s)
+end_time=$((start_time + timeout_seconds))
+while (($(date +%s) < end_time)); do
+  if cast block-number --rpc-url "${L2_RPC_URL}"; then
+    exit 0
+  fi
+  sleep 5
+done
+
+echo "Script failed after ${timeout_seconds} seconds. Exiting."
+exit 1
