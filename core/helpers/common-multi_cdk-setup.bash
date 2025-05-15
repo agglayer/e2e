2279a135fe54c67d4a759b48aa141f7b6c9e140a
Stefan Negovanović <stefan@ethernal.tech>
2025-05-15 09:22:16 +0200

    fix: resolve rpc url and provide to find imported ger tool

--- core/helpers/common-multi_cdk-setup.bash
+++ core/helpers/common-multi_cdk-setup.bash
@@ -5,8 +5,9 @@ _common_multi_setup() {
     readonly eth_address=$(cast wallet address --private-key $private_key)
     readonly l2_pp1_url=$(kurtosis port print $ENCLAVE cdk-erigon-rpc-001 rpc)
     readonly l2_pp2_url=$(kurtosis port print $ENCLAVE cdk-erigon-rpc-002 rpc)
-    readonly aggkit_pp1_node_url=$(kurtosis port print $ENCLAVE cdk-node-001 rest)
-    readonly aggkit_pp2_node_url=$(kurtosis port print $ENCLAVE cdk-node-002 rest)
+    readonly aggkit_pp1_rest_url=$(kurtosis port print $ENCLAVE cdk-node-001 rest)
+    readonly aggkit_pp2_rest_url=$(kurtosis port print $ENCLAVE cdk-node-002 rest)
+    readonly aggkit_pp1_rpc_url=$(kurtosis port print $ENCLAVE cdk-node-001 rpc)
     readonly l2_pp1_network_id=$(cast call --rpc-url $l2_pp1_url $l1_bridge_addr 'networkID() (uint32)')
     readonly l2_pp2_network_id=$(cast call --rpc-url $l2_pp2_url $l2_bridge_addr 'networkID() (uint32)')
 
@@ -16,6 +17,7 @@ _common_multi_setup() {
     echo "=== L1 RPC URL=$l1_rpc_url ===" >&3
     echo "=== L2 PP1 URL=$l2_pp1_url ===" >&3
     echo "=== L2 PP2 URL=$l2_pp2_url ===" >&3
-    echo "=== Aggkit PP1 URL=$aggkit_pp1_node_url ===" >&3
-    echo "=== Aggkit PP2 URL=$aggkit_pp2_node_url ===" >&3
+    echo "=== Aggkit PP1 REST URL=$aggkit_pp1_rest_url ===" >&3
+    echo "=== Aggkit PP2 REST URL=$aggkit_pp2_rest_url ===" >&3
+    echo "=== Aggkit PP1 RPC URL=$aggkit_pp1_rpc_url ===" >&3
 }
