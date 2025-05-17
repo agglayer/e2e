07bdd05718820b2928ecfa0a6449926dafd7d05f
Stefan Negovanović <stefan@ethernal.tech>
2025-05-17 08:47:30 +0200

    chore: format

--- core/helpers/scripts/kurtosis-helpers.bash
+++ core/helpers/scripts/kurtosis-helpers.bash
@@ -8,11 +8,11 @@ function kurtosis_download_file_exec_method() {
         echo "Error: file_path parameter is not set." >&2
         return 1
     fi
-   kurtosis service exec "$_enclave" "$_service" "cat $_file_path" | kurtosis_filer_exec_method
+    kurtosis service exec "$_enclave" "$_service" "cat $_file_path" | kurtosis_filer_exec_method
 }
 
 function kurtosis_filer_exec_method() {
-    local _kurtosis_version=$(kurtosis version | cut -d ':'  -f 2 | head -n 1)
+    local _kurtosis_version=$(kurtosis version | cut -d ':' -f 2 | head -n 1)
     # versions previous 1.7.0 first line in stdout is
     # "The command was successfully executed and returned '0'."
     # After this version this line is output in stderr
