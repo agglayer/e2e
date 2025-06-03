94aeb4a59c91e8b737ffa37dcf5b4d893d626e57
Arpit Temani <temaniarpit27@gmail.com>
2025-06-03 14:14:36 +0530

    add 3 network flow

--- core/helpers/scripts/agglayer_network_setup.bash
+++ core/helpers/scripts/agglayer_network_setup.bash
@@ -24,8 +24,8 @@ function fund_claim_tx_manager() {
 
     echo "=== Funding bridge auto-claim  ===" >&3
     cast send --legacy --value 100ether --rpc-url $l2_pp1_url --private-key $private_key 0x5f5dB0D4D58310F53713eF4Df80ba6717868A9f8
-    cast send --legacy --value 100ether --rpc-url $l2_pp2_url --private-key $private_key 0x93F63c24735f45Cd0266E87353071B64dd86bc05
+    cast send --legacy --value 100ether --rpc-url $l2_pp2_url --private-key $private_key 0x5f5dB0D4D58310F53713eF4Df80ba6717868A9f8
     if [ $number_of_chains -eq 3 ]; then
-        cast send --legacy --value 100ether --rpc-url $l2_pp3_url --private-key $private_key 0xbBc2DaF17618c46afF711882E41Ee72490C37d18
+        cast send --legacy --value 100ether --rpc-url $l2_pp3_url --private-key $private_key 0x5f5dB0D4D58310F53713eF4Df80ba6717868A9f8
     fi
 }
