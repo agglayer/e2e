a1337154d2a919a8d5d495f0cb8237f8f4bb5619
Stefan Negovanović <stefan@ethernal.tech>
2025-07-04 13:15:17 +0200

    fix: revert ENCLAVE_NAME to ENCLAVE env var

--- core/helpers/scripts/agglayer_network_setup.bash
+++ core/helpers/scripts/agglayer_network_setup.bash
@@ -6,7 +6,7 @@ function add_network_to_agglayer() {
     local rpc_url=$2
 
     echo "=== Checking if network $network_id is added to agglayer ===" >&3
-    local _prev=$(kurtosis service exec $ENCLAVE_NAME agglayer "grep \"$network_id = \" /etc/zkevm/agglayer-config.toml || true" | kurtosis_filer_exec_method)
+    local _prev=$(kurtosis service exec $ENCLAVE agglayer "grep \"$network_id = \" /etc/zkevm/agglayer-config.toml || true" | kurtosis_filer_exec_method)
     if [ ! -z "$_prev" ]; then
         echo "Network $network_id is already added to agglayer" >&3
         return
@@ -14,7 +14,7 @@ function add_network_to_agglayer() {
     echo "=== Adding network $network_id to agglayer ===" >&3
     # Extract hostname from the RPC URL
     local rpc_host=$(echo "$rpc_url" | sed -E 's|^https?://([^:]+).*|\1|')
-    kurtosis service exec $ENCLAVE_NAME agglayer "sed -i 's/\[proof\-signers\]/$network_id = \"http:\/\/$rpc_host:8123\"\n\[proof-signers\]/i' /etc/zkevm/agglayer-config.toml"
-    kurtosis service stop $ENCLAVE_NAME agglayer
-    kurtosis service start $ENCLAVE_NAME agglayer
+    kurtosis service exec $ENCLAVE agglayer "sed -i 's/\[proof\-signers\]/$network_id = \"http:\/\/$rpc_host:8123\"\n\[proof-signers\]/i' /etc/zkevm/agglayer-config.toml"
+    kurtosis service stop $ENCLAVE agglayer
+    kurtosis service start $ENCLAVE agglayer
 }
