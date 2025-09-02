194750d7f4cee1537f459d9b2fd74a94f5b4f26a
leovct <l009.vincent@gmail.com>
2025-09-02 16:04:08 +0200

    chore: allow to skip deployment and/or tests

--- scenarios/pos-veblop/run.sh
+++ scenarios/pos-veblop/run.sh
@@ -1,32 +1,37 @@
 #!/bin/env bash
 set -e
 
+source ../common/load-env.sh
+load_env
+
 enclave_name="pos-veblop"
 bor_tag="0fe4b0d"
 heimdallv2_tag="0d27dfc"
 
-# Build local images
-if ! docker images --format "table {{.Repository}}:{{.Tag}}" | grep -q "local/bor:$bor_tag"; then
-  echo "Building bor:$bor_tag..."
-  git clone --branch develop https://github.com/0xPolygon/bor
-  pushd bor
-  git checkout "$bor_tag"
-  docker build -t "local/bor:$bor_tag" .
-  popd
-fi
-
-if ! docker images --format "table {{.Repository}}:{{.Tag}}" | grep -q "local/heimdall-v2:$heimdallv2_tag"; then
-  echo "Building heimdall-v2:$heimdallv2_tag..."
-  git clone --branch develop https://github.com/0xPolygon/heimdall-v2
-  pushd heimdall-v2
-  git checkout "$heimdallv2_tag"
-  docker build -t "local/heimdall-v2:$heimdallv2_tag" .
-  popd
-fi
-
 # Spin up the network
-echo "Deploying the kurtosis enclave..."
-kurtosis run --enclave "$enclave_name" --args-file params.yml github.com/0xPolygon/kurtosis-pos@d7102e27da39c91bc19540ff45a76fab392dbcca
+if [[ "$SKIP_DEPLOYMENT" != "true" ]]; then
+  # Build local images
+  if ! docker images --format "table {{.Repository}}:{{.Tag}}" | grep -q "local/bor:$bor_tag"; then
+    echo "Building bor:$bor_tag..."
+    git clone --branch develop https://github.com/0xPolygon/bor
+    pushd bor
+    git checkout "$bor_tag"
+    docker build -t "local/bor:$bor_tag" .
+    popd
+  fi
+
+  if ! docker images --format "table {{.Repository}}:{{.Tag}}" | grep -q "local/heimdall-v2:$heimdallv2_tag"; then
+    echo "Building heimdall-v2:$heimdallv2_tag..."
+    git clone --branch develop https://github.com/0xPolygon/heimdall-v2
+    pushd heimdall-v2
+    git checkout "$heimdallv2_tag"
+    docker build -t "local/heimdall-v2:$heimdallv2_tag" .
+    popd
+  fi
+
+  echo "Deploying the kurtosis enclave..."
+  kurtosis run --enclave "$enclave_name" --args-file params.yml github.com/0xPolygon/kurtosis-pos@d7102e27da39c91bc19540ff45a76fab392dbcca
+fi
 
 # Wait for veblop hard fork to be enabled (block 256)
 l2_rpc_url=$(kurtosis port print "$enclave_name" "l2-el-1-bor-heimdall-v2-validator" rpc)
@@ -39,12 +44,14 @@ done
 echo "VeBLoP hardfork is now enabled (block number: $block_number)"
 
 # Run veblop tests
-echo "Running veblop tests..."
-cd ../..
-export KURTOSIS_ENCLAVE_NAME="$enclave_name"
-bats tests/pos/veblop.bats
-if [[ $? -ne 0 ]]; then
-  echo "❌ Tests failed"
-  exit 1
+if [[ "$SKIP_TESTS" != "true" ]]; then
+  echo "Running veblop tests..."
+  cd ../..
+  export KURTOSIS_ENCLAVE_NAME="$enclave_name"
+  bats tests/pos/veblop.bats
+  if [[ $? -ne 0 ]]; then
+    echo "❌ Tests failed"
+    exit 1
+  fi
+  echo "✅ Tests passed"
 fi
-echo "✅ Tests passed"
