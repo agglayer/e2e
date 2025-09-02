5bfe4dfa2c067479897b4a608f79c052c7f44158
leovct <l009.vincent@gmail.com>
2025-09-02 20:40:36 +0200

    feat: compute number of blocks per span

--- scenarios/pos-veblop/spans.sh
+++ scenarios/pos-veblop/spans.sh
@@ -28,8 +28,8 @@ echo "Fetching all spans from 1 to $latest_span_id..."
 echo
 
 # Fetch all spans from 1 to latest
-echo "ID | Start Block | End Block | Selected Producers"
-echo "---|-------------|-----------|------------------"
+echo "ID | Start Block | End Block | Block Count | Selected Producers"
+echo "---|-------------|-----------|-------------|------------------"
 for ((span_id=latest_span_id; span_id>=1; span_id--)); do
   span_data=$(curl -s "$cl_api_url/bor/spans/$span_id")
 
@@ -38,10 +38,13 @@ for ((span_id=latest_span_id; span_id>=1; span_id--)); do
     start_block=$(echo "$span_data" | jq -r '.span.start_block')
     end_block=$(echo "$span_data" | jq -r '.span.end_block')
 
+    # Calculate the number of blocks in the span
+    block_count=$((end_block - start_block + 1))
+
     # Get selected producers IDs as a comma-separated list
     producers=$(echo "$span_data" | jq -r '.span.selected_producers[].val_id' | tr '\n' ',' | sed 's/,$//')
 
-    printf "%-3s | %-11s | %-9s | %s\n" "$id" "$start_block" "$end_block" "$producers"
+    printf "%-3s | %-11s | %-9s | %-11s | %s\n" "$id" "$start_block" "$end_block" "$block_count" "$producers"
   else
     echo "Error fetching span $span_id"
   fi
