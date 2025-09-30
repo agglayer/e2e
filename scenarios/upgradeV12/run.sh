9092a7f215489e57c42a755da3f02b3771514b2f
Xavier Romero <xavier@polygon.technology>
2025-09-30 20:29:11 +0000

    fixes

--- scenarios/upgradeV12/run.sh
+++ scenarios/upgradeV12/run.sh
@@ -4,15 +4,15 @@ source ../common/load-env.sh
 load_env
 
 # l1 variables
-l1_preallocated_mnemonic=${L1_PREALLOCATED_MNEMONIC:-"giant issue aisle success illegal bike spike question tent bar rely arctic volcano long crawl hungry vocal artwork sniff fantasy very lucky have athlete"}
+l1_preallocated_mnemonic="giant issue aisle success illegal bike spike question tent bar rely arctic volcano long crawl hungry vocal artwork sniff fantasy very lucky have athlete"
 l1_preallocated_private_key=$(cast wallet private-key --mnemonic "$l1_preallocated_mnemonic")
 l2_admin_private_key="0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625"
 l2_admin_address=$(cast wallet address --private-key "$l2_admin_private_key")
 l2_trusted_sequencer="0x5b06837A43bdC3dD9F114558DAf4B26ed49842Ed"
 
 # infra variables
-kurtosis_enclave_name=${AGL_ENCLAVE_NAME:-"upgradeV12"}
-kurtosis_repo_tag=${CDK_KURTOSIS_PACKAGE_TAG:-"v0.4.18"}
+kurtosis_enclave_name="upgradeV12"
+kurtosis_repo_tag="v0.4.18"
 
 docker_network_name="kt-$kurtosis_enclave_name"
 
@@ -52,11 +52,11 @@ kurtosis run --enclave "$kurtosis_enclave_name" "github.com/0xPolygon/kurtosis-c
                                                                                   ##       ######                                
                                                                                   ##       :####:                                
 
-kurtosis service exec "$kurtosis_enclave_name" contracts-001 "cd /opt/zkevm-contracts/ && git fetch && git stash push -m \"kurtosise\" && git checkout v12.1.0 && git stash pop"
+kurtosis service exec "$kurtosis_enclave_name" contracts-001 "cd /opt/zkevm-contracts/ && git fetch && git stash push -m \"kurtosis\" && git checkout v12.1.0 && git stash pop"
 kurtosis service exec "$kurtosis_enclave_name" contracts-001 "cd /opt/zkevm-contracts/ && npm i"
 kurtosis service exec "$kurtosis_enclave_name" contracts-001 "echo DEPLOYER_PRIVATE_KEY="$l2_admin_private_key" > /opt/zkevm-contracts/.env"
 
-rollup_manager_address=$(1| | jq -r .polygonRollupManagerAddress)
+rollup_manager_address=$(kurtosis service exec "$kurtosis_enclave_name" contracts-001 "cat /opt/zkevm/combined-001.json | jq -r .polygonRollupManagerAddress")
 upgrade_parameters='{
     "tagSCPreviousVersion": "v1.1.0",
     "rollupManagerAddress": "'$rollup_manager_address'",
